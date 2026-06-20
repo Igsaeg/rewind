73e6e88c3b1857f99157e4384f7cb8f6924fd973
Igsaeg <kharlfranco26@gmail.com>
2026-06-20 23:36:55 +0800

    fixed

--- resize-script.js
+++ resize-script.js
@@ -3,10 +3,10 @@ import fs from 'fs';
 
 const FOLDERS = ['g7', 'g8', 'g9', 'g10'];
 const SIZES = [
-  { suffix: 'thumb', width: 40, quality: 50 },
-  { suffix: 'small', width: 250, quality: 70 },
-  { suffix: 'medium', width: 500, quality: 75 },
-  { suffix: 'large', width: 800, quality: 80 }
+  { suffix: '_thumb', width: 40, quality: 50 },
+  { suffix: '_small', width: 250, quality: 70 },
+  { suffix: '_medium', width: 500, quality: 75 },
+  { suffix: '_large', width: 800, quality: 80 }
 ];
 
 (async () => {
--- src/assets/imageData.js
+++ src/assets/imageData.js
@@ -1,24 +1,40 @@
-const grades = [7, 8, 9, 10];
-const sizes = ['thumb', 'small', 'medium', 'large'];
-
-const loadImages = () => {
-  const result = {};
-  
-  grades.forEach(grade => {
-    const sizeImages = sizes.map(size => 
-      Object.values(import.meta.glob(`./${grade}${size}/*.jpg`, { eager: true }))
-        .map(m => m.default)
-    );
-    
-    result[grade] = sizeImages[0].map((thumb, i) => ({
-      thumb: sizeImages[0][i],
-      small: sizeImages[1][i],
-      medium: sizeImages[2][i],
-      large: sizeImages[3][i]
-    }));
-  });
-  
-  return result;
+const globs = {
+  7: {
+    thumb: import.meta.glob('./g7_thumb/*.jpg', { eager: true }),
+    small: import.meta.glob('./g7_small/*.jpg', { eager: true }),
+    medium: import.meta.glob('./g7_medium/*.jpg', { eager: true }),
+    large: import.meta.glob('./g7_large/*.jpg', { eager: true })
+  },
+  8: {
+    thumb: import.meta.glob('./g8_thumb/*.jpg', { eager: true }),
+    small: import.meta.glob('./g8_small/*.jpg', { eager: true }),
+    medium: import.meta.glob('./g8_medium/*.jpg', { eager: true }),
+    large: import.meta.glob('./g8_large/*.jpg', { eager: true })
+  },
+  9: {
+    thumb: import.meta.glob('./g9_thumb/*.jpg', { eager: true }),
+    small: import.meta.glob('./g9_small/*.jpg', { eager: true }),
+    medium: import.meta.glob('./g9_medium/*.jpg', { eager: true }),
+    large: import.meta.glob('./g9_large/*.jpg', { eager: true })
+  },
+  10: {
+    thumb: import.meta.glob('./g10_thumb/*.jpg', { eager: true }),
+    small: import.meta.glob('./g10_small/*.jpg', { eager: true }),
+    medium: import.meta.glob('./g10_medium/*.jpg', { eager: true }),
+    large: import.meta.glob('./g10_large/*.jpg', { eager: true })
+  }
 };
 
-export const album = loadImages();
+const toArray = (glob) => Object.values(glob).map(m => m.default);
+
+export const album = Object.fromEntries(
+  Object.entries(globs).map(([grade, sizes]) => [
+    grade,
+    toArray(sizes.thumb).map((thumb, i) => ({
+      thumb: toArray(sizes.thumb)[i],
+      small: toArray(sizes.small)[i],
+      medium: toArray(sizes.medium)[i],
+      large: toArray(sizes.large)[i]
+    }))
+  ])
+);
