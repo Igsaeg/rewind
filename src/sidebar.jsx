c0e35f7072fa83430a5bb11618f446099e974b26
Igsaeg <kharlfranco26@gmail.com>
2026-06-21 22:59:48 +0800

    fix responsiveness

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -17,16 +17,6 @@ export default function SideBar({ selected, setSelected }) {
                     >
                         {grade}
                     </button>
-
-                    {i < grades.length - 1 && (
-                        <svg viewBox="5 11 10 10" key={`line-${i}`} className={selected > grade ? styles['past-line'] : ""} fill="none">
-                            <path
-                                d="M10,11 C7,14 13,17 10,20 C7,23 13,26 10,29"
-                                strokeWidth="0.6"
-                                strokeLinecap="round"
-                            />
-                        </svg>
-                    )}
                 </>
             ))}
         </nav>
